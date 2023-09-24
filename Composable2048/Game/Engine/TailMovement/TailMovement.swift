013d358e2b7401c18995c1f9aab208116c93929d
tornikegomareli <gomarelidevelopment@gmail.com>
2023-09-25 00:45:41 +0400

    wip

---
+++ Composable2048/Game/Engine/TailMovement/TailMovement.swift
@@ -0,0 +1,13 @@
+//
+//  TailMovement.swift
+//  Composable2048
+//
+//  Created by Tornike on 24/09/2023.
+//
+
+import Foundation
+
+struct TileMovement {
+  let from: (row: Int, col: Int)
+  let to: (row: Int, col: Int)
+}
